ea8bd9c81304f40ea199c9e0d0bf6cc8eebe4fa2
alvine <alvinecom2018@gmail.com>
2022-01-17 13:08:46 +0700

    defining API donasi and fixing API users

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -1,4 +1,5 @@
 const Users = require('../models/users.model.js');
+const bcrypt = require('bcryptjs');
 
 // Create and Save a new Note
 exports.create = (req, res) => {
@@ -7,9 +8,16 @@ exports.create = (req, res) => {
         nama: req.body.nama,
         nohp: req.body.nohp,
         email: req.body.email,
-        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, 8),
         foto: req.body.foto
     });
+
+    // bcrypt.genSalt(8, (err,salt) => {
+    //     bcrypt.hash(user.password,salt,(err,hash) => {
+    //         if(err) throw err;
+    //             user.save()
+    //     })
+    // })
 
     // Save Note in the database
     users.save()
@@ -21,6 +29,25 @@ exports.create = (req, res) => {
             });
         });
 };
+
+exports.onLogin = (req,res) => {
+    Users.findOne({email: req.body.email})
+    .then(users => {
+        if(!users){
+            return res.status(404).send({
+                message: "User not found with email "
+            });  
+        } else {
+            bcrypt.compare(req.body.password, users.password, (err, result) => {
+                if(result == true){
+                    res.status(200).send({Success: "Login Ok"})
+                } else {
+                    res.status(500).send({Failed: "Email or Password Wrong"})
+                }
+            })
+        }
+    })
+}
 
 // Retrieve and return all notes from the database.
 exports.findAll = (req, res) => {
@@ -79,6 +106,29 @@ exports.findOne = (req, res) => {
                 message: "Error retrieving users with id " + req.params.usersId
             });
         });
+};
+
+// Find a single note with a noteId
+exports.findOneEmail = (req, res) => {
+    const emails = req.params.emails;
+    Users.findOne({"email" : emails})
+    .then(users => {
+        if(!users) {
+            return res.status(404).send({
+                message: "Email not found with id " + req.params.emails
+            });            
+        }
+        res.send(users);
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "users not found with id " + req.params.emails
+            });                
+        }
+        return res.status(500).send({
+            message: "Error retrieving users with id " + req.params.emails
+        });
+    });
 };
 
 // Delete a note with the specified noteId in the request
--- app/routes/users.routes.js
+++ app/routes/users.routes.js
@@ -4,11 +4,16 @@ module.exports = (app) => {
     // Create a new Note
     app.post('/users', users.create);
 
+    // Login
+    app.post('/users/login', users.onLogin);
+
     // Retrieve all users
     app.get('/users', users.findAll);
 
     // Retrieve all users
     app.get('/users/:usersId', users.findOne);
+    app.get('/users/mail/:emails', users.findOneEmail);
+
 
     // Delete Chat
     app.delete('/users/:usersId', users.delete)
